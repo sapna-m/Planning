18e72fd9261176a89588f8f8a605d99b0e973c62
Sapna <sapna.maheswari@gmail.com>
2011-04-14 14:34:17 +0530

    Adding the skeleton structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReleasePlanning::Application.config.secret_token = '4b0b8370f9f7292fa1cedf37f130f3da7f19e661752e4075abcf5c043855f0f46605a58420125557625635c8898acf8addb079e725624f885151350c502e70f9'
